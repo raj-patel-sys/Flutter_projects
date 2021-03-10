be5e6b428bc6463694ea65403ee6292252a0bdf7
raj-patel-sys <patelrajj1904@gmail.com>
2021-03-10 19:20:13 +0530

    class inheritance

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 
-class Data {
-  String name;
-  String lastName;
-  int age;
+// class Data {
+//   String name;
+//   String lastName;
+//   int age;
 
   //constructor
 
@@ -14,12 +14,40 @@ class Data {
   // }
 
   //2nd method
-  Data(this.name,this.lastName,this.age);
+//   Data(this.name,this.lastName,this.age);
   
+// }
+
+// main() {
+//   var a = Data('raj', 'patel', 21);
+
+//   print('${a.name} ${a.lastName} is my name.');
+// }
+
+
+class Person{
+  String name, lastname; 
+  int age;
+
+  void showname(){
+    print(this.name);
+  }
+}
+
+class Raj extends Person {
+  int height = 177;
+
+  void showheight(){
+    print(this.height);
+  }
 }
 
 main() {
-  var a = Data('raj', 'patel', 21);
+  var a = new Person();
+  a.name = 'Raj';
 
-  print('${a.name} ${a.lastName} is my name.');
+  a.showname();
+
+  var b = new Raj();
+  b.showheight();
 }
